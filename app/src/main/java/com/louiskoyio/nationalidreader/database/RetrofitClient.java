5b1482d0de2d9c73e6093af14667aa799db95ac2
Louiskoyio <koyiolouis@gmail.com>
2020-08-22 01:04:25 +0300

    added api client

--- app/src/main/java/com/louiskoyio/nationalidreader/database/RetrofitClient.java
+++ app/src/main/java/com/louiskoyio/nationalidreader/database/RetrofitClient.java
@@ -10,7 +10,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "https://rightlight2.herokuapp.com";
+    private static final String BASE_URL = "https://valleygate-api.herokuapp.com/";
     private static Retrofit retrofit = null;
 
 
